c01b32a3e548c55f7f6d7f13bf4e2287c733451c
Nikola Stankovic <nikola@stankovic.xyz>
2018-07-11 00:26:33 +0200

    clean up

--- deployment/build-and-push.sh
+++ deployment/build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t registry.heroku.com/$APP_NAME/web .
+docker build -t registry.heroku.com/$APP_NAME/web -f deployment/Dockerfile .
 echo "$HEROKU_TOKEN" | docker login -u _ --password-stdin registry.heroku.com
 docker push registry.heroku.com/$APP_NAME/web
