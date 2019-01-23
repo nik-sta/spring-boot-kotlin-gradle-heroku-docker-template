221a9825a75bfbaf937639d75ab7ade95ce3399d
Nikola Stankovic <nikola@stankovic.xyz>
2019-01-23 19:20:31 +0100

    make it run again

--- src/main/kotlin/xyz/stankovic/nikola/gradlekotlinspringbootherokutemplate/GradleKotlinSpringBootHerokuTemplateApplication.kt
+++ src/main/kotlin/xyz/stankovic/nikola/gradlekotlinspringbootherokutemplate/GradleKotlinSpringBootHerokuTemplateApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GradleKotlinSpringBootHerokuTemplateApplication
+open class GradleKotlinSpringBootHerokuTemplateApplication
 
 fun main(args: Array<String>) {
     runApplication<GradleKotlinSpringBootHerokuTemplateApplication>(*args)
