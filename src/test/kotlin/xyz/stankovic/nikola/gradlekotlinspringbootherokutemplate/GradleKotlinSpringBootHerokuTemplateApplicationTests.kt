fa064b9208288cea1d9e8634067a8b383af711bb
Nikola Stankovic <nikola@stankovic.xyz>
2019-02-07 22:06:24 +0100

    remove test

--- src/test/kotlin/xyz/stankovic/nikola/gradlekotlinspringbootherokutemplate/GradleKotlinSpringBootHerokuTemplateApplicationTests.kt
+++ src/test/kotlin/xyz/stankovic/nikola/gradlekotlinspringbootherokutemplate/GradleKotlinSpringBootHerokuTemplateApplicationTests.kt
@@ -1,16 +1,11 @@
 package xyz.stankovic.nikola.gradlekotlinspringbootherokutemplate
 
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
-
-@RunWith(SpringRunner::class)
-@SpringBootTest
-class GradleKotlinSpringBootHerokuTemplateApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//@RunWith(SpringRunner::class)
+//@SpringBootTest
+//class GradleKotlinSpringBootHerokuTemplateApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
